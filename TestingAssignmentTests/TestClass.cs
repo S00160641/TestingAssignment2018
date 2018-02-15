95d5ce1153102a417b09d97e716d1bf87d468274
Sophia Price <sophiaprice10@hotmail.com>
2018-02-15 20:42:40 +0000

    Added Program

--- TestingAssignmentTests/TestClass.cs
+++ TestingAssignmentTests/TestClass.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestingAssignment
+namespace TestingAssignmentTests
 {
-    public class Class1
+    public class TestClass
     {
     }
 }
